37abe67388cd8640466e5df7efe673b1a7dabd3e
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-01-25 23:01:32 -0300

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest{
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
