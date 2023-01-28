1d8e94728289b6304afd54521db830169d08e5f1
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-01-27 21:01:05 -0300

    feat: criando teste do login

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,21 @@
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class LoginTest {
 
     @BeforeEach
     void setUp() throws Exception {
+
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+
+    }
+
+    @Test
+    void test() {
 
     }
 }
